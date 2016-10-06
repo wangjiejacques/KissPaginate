673041c0e3e621a4bb5c250db9cf9a88294df1e9
WANG Jie <jacques09125715@gmail.com>
2016-10-06 12:02:42 +0200

    update non code file

--- Example/KissPaginate/ViewController.swift
+++ Example/KissPaginate/ViewController.swift
@@ -50,13 +50,3 @@ extension ViewController: UITableViewDataSource {
         return cell
     }
 }
-
-
-func delay(delay: Double = 0, closure: ()->()) {
-    dispatch_after(
-        dispatch_time(
-            DISPATCH_TIME_NOW,
-            Int64(delay * Double(NSEC_PER_SEC))
-        ),
-        dispatch_get_main_queue(), closure)
-}
